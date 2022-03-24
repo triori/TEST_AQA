9a685769d03318fb530e4af44ec02b01e018a401
Slava <triori@gmail.com>
2022-03-24 11:27:37 +0200

    grammar corrections

--- cypress/integration/TEST-2(DB).js
+++ cypress/integration/TEST-2(DB).js
@@ -2,11 +2,11 @@ describe('Task Command', () => {
     function printResults(rows, i, separator){
         cy.log(`${rows[i].name} ${separator} ${rows[i].population / rows[i].area}`)
     }
-    it('Population desinty', () => {
+    it('Population density', () => {
         const query = 'select * from Countries';
         cy.task('Countries', query).then((rows) => {
             for (var i = 0; i < rows.length; i++) {
-                rows[i].population / rows[i].area < 50 && rows[i].name === 'USA' ? printResults(rows, i, ' population desinty is ') :  rows[i].population / rows[i].area > 50 ? printResults(rows, i, ' population desinty is ') : expect(false).to.equal(true)
+                rows[i].population / rows[i].area < 50 && rows[i].name === 'USA' ? printResults(rows, i, ' population density is ') :  rows[i].population / rows[i].area > 50 ? printResults(rows, i, ' population density is ') : expect(false).to.equal(true)
             }
             expect(true).to.equal(true)
         });
